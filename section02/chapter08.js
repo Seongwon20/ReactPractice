dd801f11d28a7013ba686ac736259f1816909e34
Seongwon20 <98306482+Seongwon20@users.noreply.github.com>
2025-01-08 00:26:43 +0900

    Add files via upload

---
+++ section02/chapter08.js
@@ -0,0 +1,64 @@
+// 5가지 요소 순회 및 탐색 메서드
+// 1. forEach
+// 모든 요소를 순회하면서, 각각의 요소에 특정 동작을 수행시키는 메서드
+let arr1=[1,2,3];
+
+arr1.forEach(function(item,idx,arr){ // (1,0) (2,1) (3,2)
+// console.log(idx,item*2);
+});
+
+let doubledArr=[];
+
+arr1.forEach((item)=>{
+    doubledArr.push(item*2);
+});
+
+//console.log(doubledArr);
+
+// 2. includes
+// 배열에 특정 요소가 있는지 확인하는 그런 메서드
+let arr2=[1,2,3];
+let isInclude=arr2.includes(3);
+
+//console.log(isInclude); 
+
+let objectArr=[
+    {name:"진성원"},
+    {name: "홍길동"},
+];
+
+// console.log(
+// objectArr.indexOf({name:"진성원"}) // indexof로 못찾음 -1이 나옴. indexof로는 객체값 못 찾음음
+// );
+
+// console.log(
+//     objectArr.findIndex(
+//         (item)=>item.name==="진성원" // 0이라고 위치가 나옴. 
+//     )
+// );
+
+//3. indexof
+//특정 요소의 인덱스(위치)를 찾아서 반환하는 메서드
+let arr3=[1,2,3];
+let index=arr3.indexOf(2);
+//console.log(index);
+
+//4. findIndex
+//모든 요소를 순회하면서, 콜백함수를 만족하는 그런
+//특정 요소의 인덱스(위치)를 반환하는 메서드
+let arr4=[1,2,3];
+const findedIndex=arr4.findIndex((item)=>item%2!==0);
+
+// console.log(findedIndex);
+
+//5. find
+// 모든 요소를 순회하면서 콜백함수를 만족하는 요소를 찾는데, 요소를 그대로 반환
+
+let arr5=[
+    {name:"진성원"},
+    {name: "홍길동"},
+];
+
+const finded=arr5.find((item)=>item.name==="진성원");
+
+console.log(finded);
